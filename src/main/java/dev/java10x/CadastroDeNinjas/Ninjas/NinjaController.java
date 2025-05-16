562acde79be8da28c03fc4535f65a34a2940ed99
Rafael Augusto Gelain <rafael.augustogelain2@gmail.com>
2025-05-15 21:01:03 -0300

    Modelagem do Banco de Dados, NinjaModel e MissoesModel Concluidos, annotations feitas para relacionamento de tabelas.

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -0,0 +1,14 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa e minha primeira mensagem nessa rota";
+    }
+    
+}
